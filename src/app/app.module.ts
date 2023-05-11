1dc7f758f55943c3cc217d2f1d869bdc29f59ffc
sergio Arroyo <chupi1987@hotmail.com>
2023-05-12 01:59:04 +0200

    angula1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { HomeComponent } from './page/home/home.component';
+import { FormRegisterComponent } from './component/form-register/form-register.component';
+import { RegisterComponent } from './page/register/register.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    FormRegisterComponent,
+    RegisterComponent,
+  
   ],
   imports: [
     BrowserModule,
