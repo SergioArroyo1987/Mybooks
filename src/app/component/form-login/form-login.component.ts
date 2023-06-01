f60d160a89535159285be04f8dba4284d63cce9e
sergio Arroyo <chupi1987@hotmail.com>
2023-06-01 20:26:04 +0200

    Formulario

--- src/app/component/form-login/form-login.component.ts
+++ src/app/component/form-login/form-login.component.ts
@@ -1,6 +1,5 @@
 import { Component,OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
+
 import {FormGroup,FormBuilder,Validators} from '@angular/forms';
 import { User } from 'src/app/models/user';
 @Component({
@@ -9,7 +8,7 @@ import { User } from 'src/app/models/user';
   styleUrls: ['./form-login.component.css']
 })
 
-export class FormLoginComponent implements OnInit {
+export class FormLoginComponent   {
 public formulario:FormGroup;
 
 constructor(private formBuilder:FormBuilder){
@@ -20,11 +19,11 @@ constructor(private formBuilder:FormBuilder){
 private buildForm(){
   this.formulario=this.formBuilder.group({
     "nombre":[,Validators.required],
-    "email":[,Validators.required,Validators.email]
+    "correo":[,[Validators.required,Validators.email]]
   });
   
 }
-ngOnInit():void{};
+
 register(){
    
   const user=this.formulario.value;
--- src/app/component/form-register/form-register.component.ts
+++ src/app/component/form-register/form-register.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import {FormGroup,FormBuilder,Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-form-register',
   templateUrl: './form-register.component.html',
   styleUrls: ['./form-register.component.css']
 })
-export class FormRegisterComponent {
+export class FormRegisterComponent implements OnInit {
+  public formulario:FormGroup;
+  constructor(private formBuilder:FormBuilder){
+    this.buildForm();
+    
+  }
+  private buildForm(){
+    this.formulario=this.formBuilder.group({
+      "nombre":[,Validators.required],
+      "correo":[,[Validators.required,Validators.email]],
+      "apellido":[,Validators.required],
+      "url":[,Validators.required],
+      "contraseña":[,[Validators.required,Validators.minLength(6)]],
+      "repetir":[,[Validators.required,Validators.minLength(6)]],
+    });
+    
+  }
+  
+  register(){
+   
+    const user=this.formulario.value;
+    console.log(user);
+    this.buildForm();
+  }
+  comprobar():boolean{
+    let devolver=false;
+    let contraseña=this.formulario.get("contraseña");
+    let repetir=this.formulario.get("repetir");
+    if(contraseña==repetir){
+      console.log(contraseña,repetir);
+      
+      devolver=true;
+    }
+    return devolver;
 
+  }
+  ngOnInit(): void {
+    
+  }
 }
